5f6b0f32f7663fe56b2d52487bec40a6a0882139
Abdelaziz Khabthani <abdelaziz.khabthani@gmail.com>
2018-08-24 01:01:51 +0100

    Rename to application

--- src/main/java/com/abdelaziz/Application.java
+++ src/main/java/com/abdelaziz/Application.java
@@ -6,9 +6,9 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 
 @SpringBootApplication
 @EnableConfigurationProperties
-public class SpringBootSampleApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootSampleApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
